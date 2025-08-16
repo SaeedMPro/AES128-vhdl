b1b46bb33c24424b7b9a5a44737ff3a796c9cebc
Yasaman <136383923+sopsit@users.noreply.github.com>
2025-08-16 12:10:52 +0330

    added packages which used in key expansion
    i used the aes_sbox which was wrote before on the other branch

---
+++ aes_rcon_pkg.vhd
@@ -0,0 +1,13 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package aes_rcon_pkg is
+    type rcon_array is array(0 to 10) of std_logic_vector(31 downto 0);
+    constant RCON : rcon_array := (
+        x"01000000", x"02000000", x"04000000", x"08000000", x"10000000",
+        x"20000000", x"40000000", x"80000000", x"1B000000", x"36000000", x"00000000"
+    );
+end package aes_rcon_pkg;
---
+++ key_expansion_utils_pkg.vhd
@@ -0,0 +1,29 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.aes_sbox_pkg.all;
+
+package key_expansion_utils_pkg is
+    function RotWord(word : std_logic_vector(31 downto 0)) return std_logic_vector;
+    function SubWord(word : std_logic_vector(31 downto 0)) return std_logic_vector;
+end package;
+
+package body key_expansion_utils_pkg is
+    function RotWord(word : std_logic_vector(31 downto 0)) return std_logic_vector is
+        variable w : std_logic_vector(31 downto 0);
+    begin
+        w := word(23 downto 0) & word(31 downto 24);
+        return w;
+    end;
+
+    function SubWord(word : std_logic_vector(31 downto 0)) return std_logic_vector is
+        variable w : std_logic_vector(31 downto 0);
+    begin
+        for i in 0 to 3 loop
+            w(8*(3-i)+7 downto 8*(3-i)) := SBOX(to_integer(unsigned(word(8*(3-i)+7 downto 8*(3-i)))));
+        end loop;
+        return w;
+    end;
+end package body;
