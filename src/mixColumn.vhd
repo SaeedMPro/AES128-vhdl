fa52b311e212b25fdf094da1bcdecb03a68e1f55
amirhosseinbt <amirhosseinbrbt@gmail.com>
2025-08-21 18:59:46 +0330

    change mixColumn

--- src/mixColumn.vhd
+++ src/mixColumn.vhd
@@ -10,12 +10,6 @@ entity mixColumn is
 end mixColumn;
 
 architecture Behavioral of mixColumn is
-	constant mixColumn_matrix : MATRIX :=(
-    (x"02", x"03", x"01", x"01"),
-    (x"01", x"02", x"03", x"01"),
-    (x"01", x"01", x"02", x"03"),
-    (x"03", x"01", x"01", x"02")
-);
 		
 begin
 		process(state_matrix)
@@ -26,22 +20,7 @@ begin
 				for j in 0 to 3 loop
 					temp := x"00";
 					for k in 0 to 3 loop
-						shifted := state_matrix(k,j)(6 downto 0)&'0';
-						if mixColumn_matrix(i,k) = x"01" then
-							temp := temp xor state_matrix(k,j);
-						elsif mixColumn_matrix(i,k) = x"02" then
-							if state_matrix(k,j)(7) = '1' then
-								temp := temp xor (x"1b" xor shifted);
-							else
-								temp := temp xor shifted;
-							end if;
-						else
-							if state_matrix(k,j)(7) = '1' then
-								temp := temp xor ((x"1b" xor shifted) xor state_matrix(k,j));
-							else
-								temp := temp xor (shifted xor state_matrix(k,j));
-							end if;
-						end if;
+							temp := temp xor mul_GF(state_matrix(k,j),mixColumn_matrix(i,k));
 					end loop;
 					output_matrix(i,j) <= temp;
 				end loop;
